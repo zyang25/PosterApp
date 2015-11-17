b3c297fe71aa9e2650357e41d1cac1f9a1c52f5a
Charles Yang <zyang7253@gmail.com>
2015-11-17 15:43:13 -0500

    C

---
+++ settings.php
@@ -0,0 +1,16 @@
+<?php
+
+// Database
+$db_connection = array();
+$db_connection['host'] = "localhost";
+$db_connection['username'] = "root"; // Change this to your user
+$db_connection['password'] = ""; // randomly generated
+$db_connection['database'] = "vjoin"; // randomly generated
+
+// Timezone
+date_default_timezone_set('America/New_York');
+//Fully Qualified Domain Name
+define("SITE_HTTP", "localhost");
+//Return email address
+define("FROM_EMAIL", "vjoin@gmail.com");
+?>
