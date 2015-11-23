b24aa48931096ac3e0869c7dbefbffd231c3c52e
Charles Yang <zyang7253@gmail.com>
2015-11-23 14:11:00 -0500

    Change password

--- Authentication/forgot_password.php
+++ Authentication/forgot_password.php
@@ -9,27 +9,30 @@ echo '<br/><br/><pre>Session_variable:<br/>';
 	var_dump($_SESSION);
 echo '</pre>';
 
-if(isset($_POST['email'])&&isset($_POST['password'])){
+if(isset($_POST['email'])){
+
 	$user = new AuthSystem();
-	$status = $user -> login($_POST['email'],$_POST['password']);
-	echo "Status: ".$status."<br/>";
-	if($status == 1 || $status == 2 || $status == 3){
-		echo "Login successfully";
+	
+	$user_array = $user->getuser($_POST['email']);
+	if($user_array!=""){
+		echo $user_array['id'];
+		$user->newpassword($user_array['email'],$user_array['id']);
 	}else{
-		echo "Email or password is not correct.";
+		echo "Bad email request.";
 	}
+
 }
+
 ?>
+
 <form role="form" method="post" action="" autocomplete="off">
 	<div class="form-group">
 		<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo $_POST['email']; } ?>" tabindex="2">
 	</div>
-	<div class="form-group">
-		<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
-	</div>
 	
-	<input type="submit" name="login" value="Login" class="btn btn-primary btn-block btn-lg" tabindex="5">
+	<input type="submit" name="submit" value="Submit" class="btn btn-primary btn-block btn-lg" tabindex="5">
 </form>
+
 <?php
 include('../static/footer.php');
 ?>
