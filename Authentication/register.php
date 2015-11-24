729b348420fe4394c9bb6528b757a48e75750162
Charles Yang <zyang7253@gmail.com>
2015-11-24 15:16:21 -0500

    JS validation

--- Authentication/register.php
+++ Authentication/register.php
@@ -37,25 +37,64 @@ echo 'Account Created, please check your email for verification information';
         <div class="row omb_row-sm-offset-3">
 			<div class="col-xs-12 col-sm-6 col-sm-offset-3">
                 <h1>Vjoin Registration</h1>
-			   <form role="form" method="post" action="" autocomplete="off">
+			   <form role="form" method="post" action="" autocomplete="off" id="register_form">
                     <div class="form-group">
-                    <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo $_POST['email']; } ?>" tabindex="2">
+                    <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo $_POST['email']; } ?>" tabindex="2" required>
+                    <span id="email_error"></span>
                     </div>
                     <div class="form-group">
-                    <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
+                    <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3" required>
                     </div>
                     <div class="form-group">
-                    <input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="4">
+                    <input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="4" required>
                     </div>
                     <input type="submit" name="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="5">
+                    <span id="password_error"></span>
                     </form>
 			</div>
     	</div>
 	</div>
-    
     <p class="text-center small"><span class="releasetext">Release: 8.5.1.2</span></p>
 
 </div>
-    
+    <!-- Javascript -->
+    <script src="../assets/js/jquery-1.11.1.min.js"></script>
+    <script>
+    function check_email(){
+        var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        var stevens_email = /^\w+@(stevens.edu)$/;
+        var email = document.getElementById("email").value;
+        var r = stevens_email.test(email);
+        document.getElementById("email_error").innerHTML = "Please use Stevens edu email.";
+        console.log(r);
+        if (r==true){
+            document.getElementById("email_error").innerHTML = "";
+        }
+        return r;
+    };
+    function check_password(){
+        var password1 = document.getElementById("password").value;
+        var password2 = document.getElementById("passwordConfirm").value;
+        if(password1.length<6||password2.length<6){
+            document.getElementById("password_error").innerHTML = "Password error";
+            return false;
+        }else if(password1 != password2){
+            document.getElementById("password_error").innerHTML = "Password does not match.";
+            return false;
+        }else{
+            document.getElementById("password_error").innerHTML = "";
+            return true;
+        }
+    };
+    $('#register_form').bind("submit",function(e){
+        if(check_email()&&check_password()){
+            console.log("Hello");
+            $(this).unbind('submit').submit();
+        }else{
+            e.preventDefault();
+        }
+    });
+</script>
+
 </body>
 </html>
