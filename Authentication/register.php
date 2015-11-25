b10c6622b66a75eccb648f6062975684c69dfc4b
yubousa10 <yubousa10@163.com>
2015-11-25 11:09:14 -0500

    alert style and exit

--- Authentication/register.php
+++ Authentication/register.php
@@ -10,7 +10,10 @@ echo '</pre>';
 if(isset($_POST['email'])&&isset($_POST['password'])){
 $user = new AuthSystem();
 $user -> createuser($_POST['email'],$_POST['password']);
-echo 'Account Created, please check your email for verification information';
+echo "<div class=\"alert alert-success\" role=\"alert\">";
+echo "Account Created, please check your email for verification information";
+echo "<a href=\"../index.php\" accesskey=\"3\" class=\"submenulinktext2\"><i class=\"fa fa-sign-out\"></i> Exit</a></p>";
+echo "</div>";
 }
 
 ?>
@@ -27,16 +30,13 @@ echo 'Account Created, please check your email for verification information';
 <div class="container">
     
 <div class="row">
-<img style="margin-top:-10px;"class="pull-left" src="../assets/img/stevens.png" alt="UAH"/>
-<p style="margin:14px 20px 0 0;" class="pull-right">
-    <a href="http://www.uah.edu/oit/getting-started/for-students/67-main/its/666-campus-ids" accesskey="H" onclick="popup = window.open('http://www.uah.edu/oit/getting-started/for-students/67-main/its/666-campus-ids', 'PopupPage','height=500,width=450,scrollbars=yes,resizable=yes'); return false" target="_blank" onmouseover="window.status='';  return true" onmouseout="window.status=''; return true" onfocus="window.status='';  return true" onblur="window.status=''; return true" class="submenulinktext2"><i class="fa fa-question-circle"></i> Help</a>  |  
-    <a href="bwskalog.p_displogoutnon" accesskey="3" class="submenulinktext2"><i class="fa fa-sign-out"></i> Exit</a></p>
+<img style="margin-top:-10px;width:300px;"class="pull-left" src="../assets/img/stevens.png" alt="UAH"/>
 </div>
     <div class="omb_login">
 
         <div class="row omb_row-sm-offset-3">
 			<div class="col-xs-12 col-sm-6 col-sm-offset-3">
-                <h1>Vjoin Registration</h1>
+                <h1>Vjoin SignUp</h1>
 			   <form role="form" method="post" action="" autocomplete="off" id="register_form">
                     <div class="form-group">
                     <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo $_POST['email']; } ?>" tabindex="2" required>
@@ -48,7 +48,7 @@ echo 'Account Created, please check your email for verification information';
                     <div class="form-group">
                     <input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="4" required>
                     </div>
-                    <input type="submit" name="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="5">
+                    <input type="submit" name="submit" value="Register" class="btn btn-danger btn-block btn-lg" tabindex="5">
                     <span id="password_error"></span>
                     </form>
 			</div>
