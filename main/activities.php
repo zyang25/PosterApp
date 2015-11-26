695efa17855cfd14211aec8ec9f5c244bcbc2dcc
funkyxym <funkyxym@Yangmings-MacBook-Pro.local>
2015-11-26 16:25:25 -0500

    update my part

--- main/activities.php
+++ main/activities.php
@@ -2,54 +2,37 @@
 if(!isset($_SESSION['user_id'])){
     header('Location: ../index.php');
 }
-?>
-<html>
-    <head>
-        Activities
-    </head>
-    <body>
+?>              
         <!-- Page Content -->
             <div class="container">
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header" id = "activity_name">Name need to be rendered
-                            <small>Secondary Text</small>
+                        <h1 class="page-header" id = "activity_name"> Activity Name need to be rendered
+                            <small id = "category_name">category Name need to be rendered</small>
                         </h1>
                     </div>
                 </div>
-                <!-- /.row -->
-                <div class="btn-group btn-group-justified" role="group" aria-label="...">
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-default">eat</button>
-                    </div>
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-default">play</button>
-                    </div>
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-default">study</button>
-                    </div>
-                </div>
                 
-
-                <!-- Project One -->
+                
                 <div class="row">
                     <div class="col-md-7">
-                        <a href="#">
-                            <img class="img-responsive" src="../assets/img/activites/BBQ.jpg" alt="">
-                        </a>
+                            <img class="img-responsive" id = "image_area" src="../assets/img/activites/BBQ.jpg" alt="">
+                            <!-- /.row modify this image src from database !!!!-->
                     </div>
-                    <div class="col-md-5">
-                        <h3>BBQ on campus & music</h3>
-                        <h3>Babbio Center 104</h3>
-                        <h4>Nov. 8, 2015, 6:47 p.m</h4>
-                        <p>Had the brisket and chicken, very tasty and moist.</p>
-                        <a class="btn btn-primary" href="#">follow</a>&nbsp;&nbsp;&nbsp;&nbsp;<span>45/60</span>
+                    <div class="col-md-5">                             
+                        <h3 id = "location">Babbio Center 104</h3>  <!-- location-->
+                        <h4 id = "time_stamp">Nov. 8, 2015, 6:47 p.m</h4> <!-- time-->
+                        <p id = "description">Had the brisket and chicken, very tasty and moist.</p>      <!-- description-->
+                        <br>
+
+                        <a class="btn btn-primary" id = "follow_status" value = "unfollow">   follow</a>&nbsp;&nbsp;&nbsp;&nbsp;<span id = "remaining">45/60</span>
+                    
                     </div>
                 </div>
                 <!-- /.row -->
 
-                <!-- Pagination -->
+                <!-- Pagination handle multiple images-->
                 <div class="row text-center">
                     <div class="col-lg-12">
                         <ul class="pagination">
@@ -85,7 +68,7 @@ if(!isset($_SESSION['user_id'])){
                 <footer>
                     <div class="row">
                         <div class="col-lg-12">
-                            <p>Copyright &copy; Your Website 2015</p>
+                            <p>Copyright &copy; Vjoin 2015</p>
                         </div>
                     </div>
                     <!-- /.row -->
