ec201bc53464b485e5de2669a727599e2d427053
yubousa10 <yubousa10@163.com>
2015-11-29 20:08:24 -0500

    add button

--- main/index.php
+++ main/index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once('data.php');
+require_once('addActivity.php');
 if(!isset($_SESSION['user_id'])){
 header('Location: ../index.php');
 }
--- main/nav.php
+++ main/nav.php
@@ -16,6 +16,16 @@
           <ul class="nav navbar-nav">
             <li class="active"><a href="">Home</a></li>
           </ul>
+          <ul class="nav navbar-nav">
+             <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">postManager <span class="caret"></span></a>
+             <ul class="dropdown-menu">
+               <li><a data-toggle="modal" data-target="#myModal">Post a activity</a></li>
+               <li><a href="./editActivity.php">Manage post activities</a></li>
+             </ul>
+             </li>
+          </ul>
+
           <ul class="nav navbar-nav navbar-right">
             <?php
             if(isset($_SESSION['user_id'])){
