6bf7251d61ff8e170d96f45e1ffe5828a27613cc
yubousa10 <yubousa10@163.com>
2015-12-07 20:44:27 -0500

    list style

--- chat/friendList.php
+++ chat/friendList.php
@@ -1,20 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-<script type="text/javascript">
-function color_change(val,obj){
-     if (val=='over'){
-	  obj.style.color="red";
-	  obj.style.cursor='pointer';
-	 }
-	 else if (val=='out'){
-	  obj.style.color="black";
-	 }
+        <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<style type="text/css">
+  .list-content{
+ min-height:300px;
 }
-function openChatRoom(obj){
-   window.open("chatRoom.php?username="+obj.innerHTML,"_blank");
+.list-content .list-group .title{
+  background:#5bc0de;
+  border:2px solid #DDDDDD;
+  font-weight:bold;
+  color:#FFFFFF;
 }
-</script>
+.list-group-item img {
+    height:80px; 
+    width:80px;
+}
+
+.jumbotron .btn {
+    padding: 5px 5px !important;
+    font-size: 12px !important;
+}
+.prj-name {
+    color:#5bc0de;    
+}
+.break{
+    width:100%;
+    margin:20px;
+}
+.name {
+    color:#5bc0de;    
+}
+
+</style>
+
+</head>
+<body>
 <?php
       require_once '../main/data.php';
       $UserService=new User();
@@ -25,15 +46,32 @@ function openChatRoom(obj){
       //$preference=174;
       //$Email='jli75@stevens.edu';
       //$Users=$UserService->getUserNameByEmail($Email);
-	  
+    
       echo "<h1>FriendList</h1>";
+      echo "<div class='container bootstrap snippet'>";
+      echo " <div class='jumbotron list-content'>";
       echo "<ul>";
+
       foreach ($Users as $key=>$val){
-          echo "<li onmouseover=\"color_change('over',this)\" onclick='openChatRoom(this)' onmouseout=\"color_change('out',this)\">".$val['fname']." ".$val['lname']."</li>"; 
+          //echo "<li onmouseover=\"color_change('over',this)\" onclick='openChatRoom(this)' onmouseout=\"color_change('out',this)\">".$val['fname']." ".$val['lname']."</li>"; 
+          
+          echo "<li onclick=\"openChatRoom('".$val['fname']." ".$val['lname']."');\" class='list-group-item text-left'>";
+          echo "<img class='img-thumbnail' src='http://bootdey.com/img/Content/User_for_snippets.png'>";
+          echo "<label class='name'>";
+          echo "<div>".$val['fname']." ".$val['lname']."</div>"."<br>";
+          echo "</label>";
+          echo "<div class='break'></div>";
+          echo "</li>";
       }
       echo "</ul>";
+      echo "</div>";
+      echo "</div>";
 ?>
-</head>
-<body>
 </body>
+<script type="text/javascript">
+function openChatRoom(a){
+   window.open("chatRoom.php?username="+a,"_blank");
+}
+</script>
+
 </html>
