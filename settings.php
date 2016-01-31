08d7fe026db8bbc713e85959e055c7ce794c3a5d
Charles Yang <zyang7253@gmail.com>
2016-01-31 15:42:18 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,14 +1,11 @@
 <?php
 // Database
 $db_connection = array();
-$db_connection['host'] = "ec2-54-193-93-188.us-west-1.compute.amazonaws.com";
-$db_connection['username'] = "admin"; // Change this to your user
-$db_connection['password'] = "vjoin123456"; // randomly generated
+$db_connection['host'] = "54.210.240.128";
+$db_connection['username'] = "Charles"; // Change this to your user
+$db_connection['password'] = "Charles9129"; // randomly generated
 $db_connection['database'] = "cs546_final"; // randomly generated
-// $db_connection['host'] = "localhost";
-// $db_connection['username'] = "root"; // Change this to your user
-// $db_connection['password'] = "123"; // randomly generated
-// $db_connection['database'] = "final"; // randomly generated
+
 // Timezone
 date_default_timezone_set('America/New_York');
 //Fully Qualified Domain Name
